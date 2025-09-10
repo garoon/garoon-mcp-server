b0e5b358ae282bf4ef5b45a1a3ddb5244bc1c2b1
thanh-long-nguyen <thanh-long-nguyen@cybozu.vn>
2025-09-10 19:14:50 +0700

    GTM-19632: Remove unused files

--- src/tools/users/index.ts
+++
@@ -1,4 +0,0 @@
-import { Tool } from "../register.js";
-import { getGaroonUsersTool } from "../base/users/get-garoon-users/index.js";
-
-export const usersTools: Array<Tool<any, any>> = [getGaroonUsersTool];
