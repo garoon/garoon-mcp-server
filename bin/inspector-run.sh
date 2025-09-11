e888492f716b3f35d31206800e0e0930c458bbf2
Lucas Truong <lucas.truong.ex@gmail.com>
2025-09-11 14:35:22 +0700

    GTM-19607: Support secure access

--- bin/inspector-run.sh
+++ bin/inspector-run.sh
@@ -4,7 +4,15 @@ cd "$(dirname $0)/.."
 
 if [ -f .env.local ]; then
   source .env.local
-  npx @modelcontextprotocol/inspector -e GAROON_BASE_URL=${GAROON_BASE_URL} -e GAROON_USERNAME=${GAROON_USERNAME} -e GAROON_PASSWORD=${GAROON_PASSWORD} tsx src/index.ts
+  npx @modelcontextprotocol/inspector \
+    -e GAROON_BASE_URL=${GAROON_BASE_URL} \
+    -e GAROON_USERNAME=${GAROON_USERNAME} \
+    -e GAROON_PASSWORD=${GAROON_PASSWORD} \
+    -e http_proxy=${http_proxy} \
+    -e https_proxy=${https_proxy} \
+    -e GAROON_PFX_FILE_PATH=${GAROON_PFX_FILE_PATH} \
+    -e GAROON_PFX_FILE_PASSWORD=${GAROON_PFX_FILE_PASSWORD} \
+    tsx src/index.ts
 else
   npx @modelcontextprotocol/inspector tsx src/index.ts
 fi
