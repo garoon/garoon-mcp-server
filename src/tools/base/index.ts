fc8141a2889526771ceb4a5aa60f64f296db1ef2
thanh-long-nguyen <thanh-long-nguyen@cybozu.vn>
2025-09-11 16:39:34 +0700

    GTM-19632: Remove unused type

--- src/tools/base/index.ts
+++ src/tools/base/index.ts
@@ -1,6 +1,4 @@
 import { getCurrentTimeTool } from "./get-current-time/index.js";
 import { getGaroonUsersTool } from "./get-garoon-users/index.js";
 
-type BaseTool = typeof getCurrentTimeTool | typeof getGaroonUsersTool;
-
-export const baseTools: BaseTool[] = [getCurrentTimeTool, getGaroonUsersTool];
+export const baseTools = [getCurrentTimeTool, getGaroonUsersTool];
--- src/tools/schedule/index.ts
+++ src/tools/schedule/index.ts
@@ -2,12 +2,7 @@ import { createScheduleEvent } from "./create-schedule-event/index.js";
 import { searchScheduleEvents } from "./get-schedule-events/index.js";
 import { searchAvailableTimes } from "./search-available-times/index.js";
 
-type ScheduleTool =
-  | typeof createScheduleEvent
-  | typeof searchScheduleEvents
-  | typeof searchAvailableTimes;
-
-export const scheduleTools: ScheduleTool[] = [
+export const scheduleTools = [
   createScheduleEvent,
   searchScheduleEvents,
   searchAvailableTimes,
