cdb46fd5e011778a465767db5595b1ba1cf6d3d5
L2T <loc-truong@cybozu.vn>
2025-09-15 15:02:14 +0700

    GTM-19607: Update inspector-run.sh

--- bin/inspector-run.sh
+++ bin/inspector-run.sh
@@ -14,12 +14,20 @@ if [ -f .env.local ]; then
       -e GAROON_PFX_FILE_PATH=${GAROON_PFX_FILE_PATH} \
       -e GAROON_PFX_FILE_PASSWORD=${GAROON_PFX_FILE_PASSWORD} \
       tsx src/index.ts
+  elif [ -n "${http_proxy}" ] && [ -n "${https_proxy}" ]; then
+      npx @modelcontextprotocol/inspector \
+        -e GAROON_BASE_URL=${GAROON_BASE_URL} \
+        -e GAROON_USERNAME=${GAROON_USERNAME} \
+        -e GAROON_PASSWORD=${GAROON_PASSWORD} \
+        -e http_proxy=${http_proxy} \
+        -e https_proxy=${https_proxy} \
+        tsx src/index.ts
   else
-    npx @modelcontextprotocol/inspector \
-      -e GAROON_BASE_URL=${GAROON_BASE_URL} \
-      -e GAROON_USERNAME=${GAROON_USERNAME} \
-      -e GAROON_PASSWORD=${GAROON_PASSWORD} \
-      tsx src/index.ts
+      npx @modelcontextprotocol/inspector \
+        -e GAROON_BASE_URL=${GAROON_BASE_URL} \
+        -e GAROON_USERNAME=${GAROON_USERNAME} \
+        -e GAROON_PASSWORD=${GAROON_PASSWORD} \
+        tsx src/index.ts
   fi
 else
   npx @modelcontextprotocol/inspector tsx src/index.ts
