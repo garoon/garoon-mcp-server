036a527c6a8deff1ad0faf79d221314b5fdf5548
L2T <loc-truong@cybozu.vn>
2025-09-15 14:28:57 +0700

    GTM-19607: Update inspector-run.sh

--- bin/inspector-run.sh
+++ bin/inspector-run.sh
@@ -4,15 +4,23 @@ cd "$(dirname $0)/.."
 
 if [ -f .env.local ]; then
   source .env.local
-  npx @modelcontextprotocol/inspector \
-    -e GAROON_BASE_URL=${GAROON_BASE_URL} \
-    -e GAROON_USERNAME=${GAROON_USERNAME} \
-    -e GAROON_PASSWORD=${GAROON_PASSWORD} \
-    -e http_proxy=${http_proxy} \
-    -e https_proxy=${https_proxy} \
-    -e GAROON_PFX_FILE_PATH=${GAROON_PFX_FILE_PATH} \
-    -e GAROON_PFX_FILE_PASSWORD=${GAROON_PFX_FILE_PASSWORD} \
-    tsx src/index.ts
+  if [ -n "${GAROON_PFX_FILE_PATH}" ] && [ -n "${GAROON_PFX_FILE_PASSWORD}" ]; then
+    npx @modelcontextprotocol/inspector \
+      -e GAROON_BASE_URL=${GAROON_BASE_URL} \
+      -e GAROON_USERNAME=${GAROON_USERNAME} \
+      -e GAROON_PASSWORD=${GAROON_PASSWORD} \
+      -e http_proxy=${http_proxy} \
+      -e https_proxy=${https_proxy} \
+      -e GAROON_PFX_FILE_PATH=${GAROON_PFX_FILE_PATH} \
+      -e GAROON_PFX_FILE_PASSWORD=${GAROON_PFX_FILE_PASSWORD} \
+      tsx src/index.ts
+  else
+    npx @modelcontextprotocol/inspector \
+      -e GAROON_BASE_URL=${GAROON_BASE_URL} \
+      -e GAROON_USERNAME=${GAROON_USERNAME} \
+      -e GAROON_PASSWORD=${GAROON_PASSWORD} \
+      tsx src/index.ts
+  fi
 else
   npx @modelcontextprotocol/inspector tsx src/index.ts
 fi
