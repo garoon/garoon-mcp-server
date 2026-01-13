bf52318289e49ca7bf6310ec33565a5b0da69b46
Tomoyuki Shimizu <tomoyuki-shimizu@cybozu.co.jp>
2026-01-13 18:56:31 +0900

    GTM-20328: fix license error

--- license-manager.config.cjs
+++ license-manager.config.cjs
@@ -33,9 +33,35 @@ const config = createConfig({
       );
       return { licenseText: `${copyright}\n\n${licenseText}` };
     }
+    if (dep.name === "@hono/node-server") {
+      // The honojs/node-server has no LICENSE file, so we manually add the copyright notice.
+      // Instead, we use the copyright notice from the honojs/hono.
+      // ref: https://github.com/honojs/hono/blob/main/LICENSE
+      const copyright =
+        "MIT License\n\nCopyright (c) 2021 - present, Yusuke Wada and Hono contributors";
+      return { licenseText: `${copyright}\n\n${MIT_LICENSE_TEXT}` };
+    }
     return undefined;
   },
   packageManager: "pnpm",
 });
 
 module.exports = config;
+
+const MIT_LICENSE_TEXT = `Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
